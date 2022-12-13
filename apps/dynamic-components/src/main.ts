715db255cfc313f681497569abfa7ede1df6c200
xocomil <xocomil@gmail.com>
2022-12-12 18:32:28 -0700

    added angular material

--- apps/dynamic-components/src/main.ts
+++ apps/dynamic-components/src/main.ts
@@ -1,14 +1,15 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app.routes';
 import { environment } from './environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 if (environment.production) {
   enableProdMode();
 }
 
 void bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes)],
+  providers: [provideRouter(appRoutes), importProvidersFrom(BrowserAnimationsModule)],
 });
